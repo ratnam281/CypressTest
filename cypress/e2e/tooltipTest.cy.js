46d28a9e8d3a84b35a65a3d8d1ffc24a7329b7f7
ratnakar481 <sratnakar481@gmail.com>
2023-12-19 22:40:48 +0530

    after installing mocha reporter

---
+++ cypress/e2e/tooltipTest.cy.js
@@ -0,0 +1,11 @@
+describe('tooltip p[ractise', ()=> {
+    it('test',()=>{
+        cy.visit('https://practice.expandtesting.com/tooltips')
+        cy.get('#btn3').trigger('mouseover')
+        // cy.get('.tooltip-inner').invoke('text').as('textFunction')
+        cy.get('.tooltip-inner').invoke('text').then((text1)=> {
+            cy.log(text1)
+        })
+        // cy.log(data)
+    })
+})
