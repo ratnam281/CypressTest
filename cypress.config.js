18121f06e9dd7391790f77663b304e64b493d1d2
ratnakar481 <sratnakar481@gmail.com>
2024-02-21 10:06:31 +0530

    after retries

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,30 @@
 const { defineConfig } = require("cypress");
+// import { tagify } from 'cypress-tags';
 
 module.exports = defineConfig({
   video:true,
   reporter: 'cypress-mochawesome-reporter',
+  // reporterOptions: {
+  //   charts: true,
+  //   reportPageTitle: 'custom-title',
+  //   embeddedScreenshots: true,
+  //   inlineAssets: true,
+  //   saveAllAttempts: false,
+  // },
+   
+    retries: {
+      // Configure retry attempts for `cypress run`
+      // Default is 0
+      runMode: 2,
+      // Configure retry attempts for `cypress open`
+      // Default is 0
+      openMode: 2
+    },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
       require('cypress-mochawesome-reporter/plugin')(on);
+      // on('file:preprocessor', tagify(config));
     },
   },
 });
--- cypress/e2e/tooltipTest.cy.js
+++ cypress/e2e/tooltipTest.cy.js
@@ -1,11 +1,46 @@
-describe('tooltip practise', ()=> {
-    it('test',()=>{
+describe('tooltip practise',
+    // {
+    // retries: {
+    //   runMode: 2,
+    //   openMode: 2,
+    // },
+    // }, 
+  ()=> {
+    it.skip('test',
+    // {
+    // retries: {
+    //   runMode: 2,
+    //   openMode: 2,
+    // },
+    // },
+    ()=>{
         cy.visit('https://practice.expandtesting.com/tooltips')
         cy.get('#btn3').trigger('mouseover')
         // cy.get('.tooltip-inner').invoke('text').as('textFunction')
         cy.get('.tooltip-inner').invoke('text').then((text1)=> {
             cy.log(text1)
+            cy.get('#btn3').parent().invoke('attr','class').then((text2)=> 
+            {
+                    cy.log(text2) 
+            })
+              cy.title().then((title)=> 
+              { cy.log(title)
+            })
         })
-        // cy.log(data)
     })
+
+    it.only('alert',()=>{
+        cy.visit('https://the-internet.herokuapp.com/javascript_alerts')
+         
+         cy.on('window:alert',function(AlertText) // for alert type
+         
+         {expect(AlertText).eql('I am a JS Alert') 
+         })
+            
+         cy.contains('Click for JS Alert').click()
+         cy.get('#result').should('contain','You successfully clicked an alert') 
+        //  cy.screenshot()
+        // cy.log(data)
+    })     
+        
 })
